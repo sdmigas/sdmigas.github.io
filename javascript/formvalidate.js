7654b27d2952998b6069b7df14dbdd66aaf356fa
sdmigas <sd.migas@gmail.com>
2026-04-25 01:59:57 +1200

    Delete javascript directory

--- javascript/formvalidate.js
+++
@@ -1,7 +0,0 @@
-function enableCreateUser() {
-  if (document.getElementById("videocheck").checked) {
-    document.getElementById("video-link").classList.remove('disable_section')
-  } else {
-    document.getElementById("video-link").classList.add('disable_section')
-  }
-}
